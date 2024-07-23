fe584433cd99f49a7a0ffc2da889f6628cd0a57d
winnie-ma <xuma8821@gmail.com>
2024-07-24 04:27:48 +1000

    feat: realize card and header part of about

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,14 +1,34 @@
+import { mobile, backend, creator, web } from "../assets";
 export const navLinks = [
-    {
-      id: "about",
-      title: "About",
-    },
-    {
-      id: "work",
-      title: "Work",
-    },
-    {
-      id: "contact",
-      title: "Contact",
-    },
-  ];
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+export const services = [
+  {
+    title: "React Developer",
+    icon: web,
+  },
+  {
+    title: "Angular Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Team Player",
+    icon: creator,
+  },
+];
