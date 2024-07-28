7c3e18e871521aae338eaf86e68affe9b1480538
winnie-ma <xuma8821@gmail.com>
2024-07-28 19:27:16 +1000

    feat: delete useless animate for tech balls

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -9,7 +9,15 @@ const Tech = () => {
     <div className="flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((technology) => (
         <div className="w-28 h-28" key={technology.name}>
-          <BallCanvas icon={technology.icon} />
+          <img
+            className="w-full h-full"
+            src={technology.icon}
+            alt={technology.name}
+          />
+
+          <div className="flex justify-center item-center text-secondary text-[16px] font-semibold">
+            {technology.name}
+          </div>
         </div>
       ))}
     </div>
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-import { EarthCanvas, BallCanvas, StarsCanvas } from "./canvas";
+import { EarthCanvas, StarsCanvas } from "./canvas";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import About from "./About";
@@ -16,6 +16,5 @@ export {
   Works,
   Contact,
   EarthCanvas,
-  BallCanvas,
   StarsCanvas,
 };
